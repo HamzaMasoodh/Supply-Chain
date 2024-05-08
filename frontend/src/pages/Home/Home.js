1b9ab71b2061f22fba8b7317a0b01267c32cae72
Muhammad Muqrrab <muqrrab2@gmail.com>
2024-05-08 23:53:29 +0500

    Chat Link

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -22,13 +22,19 @@ const Home = ({ setuser }) => {
   return (
     <>
       <nav className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-500 relative flex flex-wrap items-center justify-between px-2 py-3 text-white rounded-xl">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+        <div className="container px-4 mx-auto ">
+          <div className="w-full relative flex justify-between lg:w-auto">
             <Link
               className="text-lg md:text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white cursor-pointer outline-none focus:outline-none"
               to="/Home"
             >
               Supply Chain Tool
+            </Link>
+            <Link
+              className="text-lg md:text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white cursor-pointer outline-none focus:outline-none"
+              to="/chat"
+            >
+              Chat
             </Link>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
